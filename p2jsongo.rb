552aa81d09063bb3b5eef16ad186f93eb6785194
LasEmil <emil.laskowski@netgutu.com>
2020-07-04 15:30:20 +0200

    Update p2jsongo.rb

--- p2jsongo.rb
+++ p2jsongo.rb
@@ -2,18 +2,18 @@ require 'rbconfig'
 class P2jsongo < Formula
   desc ""
   homepage "https://github.com/LasEmil/p2jsongo"
-  version "0.0.2"
+  version "0.0.3"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/LasEmil/p2jsongo/releases/download/v0.0.2/p2jsongo_0.0.2_darwin_amd64.zip"
-      sha256 "979f26bd4fbc2c090c2b65512b3344616efe17748f4b5970aa91ad3e7543aac0"
+      url "https://github.com/LasEmil/p2jsongo/releases/download/v0.0.3/p2jsongo_0.0.3_darwin_amd64.zip"
+      sha256 "32ca57c198f836c3ccd07e60c1c6950af638ee5601f0f21f2ef34792a80fe06d"
     when /linux/
-      url "https://github.com/LasEmil/p2jsongo/releases/download/v0.0.2/p2jsongo_0.0.2_linux_amd64.tar.gz"
-      sha256 "f4ae75d08de86202fe685a41ba57d4416cad0d183c775018055c6f94161c0266"
+      url "https://github.com/LasEmil/p2jsongo/releases/download/v0.0.3/p2jsongo_0.0.3_linux_amd64.tar.gz"
+      sha256 "ffbea050f61ff7c9591f8c08aa64153107bb963f8a3d13b15b035922d0b64231"
     when /solaris|bsd/
       :unix
     else
@@ -24,11 +24,11 @@ class P2jsongo < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/LasEmil/p2jsongo/releases/download/v0.0.2/p2jsongo_0.0.2_darwin_386.zip"
-      sha256 "5590ee6688be9049ca356df01806e58fc3006289c139f880259d56942a10b5da"
+      url "https://github.com/LasEmil/p2jsongo/releases/download/v0.0.3/p2jsongo_0.0.3_darwin_386.zip"
+      sha256 "6b67b34dd0b9010a6572ac8897cda97dbf10e707a7ab84796bd2869088a52e48"
     when /linux/
-      url "https://github.com/LasEmil/p2jsongo/releases/download/v0.0.2/p2jsongo_0.0.2_linux_386.tar.gz"
-      sha256 "8fbce567c36275fdab77daff7ca875f40cfc2db4ba6237a304a8c3c7b4ce671d"
+      url "https://github.com/LasEmil/p2jsongo/releases/download/v0.0.3/p2jsongo_0.0.3_linux_386.tar.gz"
+      sha256 "ed464fc16d3c239c3241728b2601ed0fc806338ed78a38623d60214b1fe7f798"
     when /solaris|bsd/
       :unix
     else
